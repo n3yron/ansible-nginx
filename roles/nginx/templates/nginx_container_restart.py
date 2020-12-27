094ec331fa276190138d6ff94cb513279214db35
n3yron <n3yron@gmail.com>
2020-12-27 11:48:31 +0200

    asdsdsad

--- roles/nginx/templates/nginx_container_restart.py
+++ roles/nginx/templates/nginx_container_restart.py
@@ -5,9 +5,11 @@ proc = subprocess.Popen(['docker', 'ps'], stdout=subprocess.PIPE)
 output = proc.stdout.readlines()
 for line in output:
     if "nginx" in line:
-#        print(line[0:12])
         nginx_container = line[0:12]
-
-print(nginx_container)
-
+print("Obtaining Nginx container id")
+print("Your Nginx container id is: "+nginx_container)
+print("Renew certs for hosts")
+print(os.system('docker exec -d {} certbot --nginx -d aws.n3yron.ru -d petclinic.n3yron.ru --force-renewal -n'.format(nginx_container)))
+print("Reloading Nginx")
 print(os.system('docker exec -d {} nginx -s reload'.format(nginx_container)))
+#certbot --nginx -d aws.n3yron.ru -d petclinic.n3yron.ru --force-renewal -n
