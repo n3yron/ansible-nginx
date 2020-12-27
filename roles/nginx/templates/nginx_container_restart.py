2cb0cf25e1182c64e463b6b2ca34299bbd2314bf
n3yron <n3yron@gmail.com>
2020-12-27 11:56:27 +0200

    adasdasasd

--- roles/nginx/templates/nginx_container_restart.py
+++ roles/nginx/templates/nginx_container_restart.py
@@ -13,5 +13,7 @@ print("Renew certs for hosts")
 print("Reloading Nginx")
 print(os.system('docker exec -d {} nginx -s reload'.format(nginx_container)))
 proc1 = subprocess.Popen(['docker', 'exec', '-d', nginx_container, 'certbot', '--nginx', '-d', 'aws.n3yron.ru', '-d', 'petclinic.n3yron.ru', '--force-renewal', '-n'], stdout=subprocess.PIPE)
-print(proc1)
+output1 = proc1.stdout.readlines()
+print(output1)
+
 #certbot --nginx -d aws.n3yron.ru -d petclinic.n3yron.ru --force-renewal -n
