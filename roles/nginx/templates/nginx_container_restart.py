23b48281a0207a6fd41c422104d736b906bb1339
n3yron <n3yron@gmail.com>
2020-12-29 23:20:42 +0200

    files

--- roles/nginx/templates/nginx_container_restart.py
+++ roles/nginx/templates/nginx_container_restart.py
@@ -8,14 +8,16 @@ for line in output:
         nginx_container = line[0:12]
 print("Obtaining Nginx container id")
 print("Your Nginx container id is: "+nginx_container)
-print("Renew certs for hosts")
+#print("Renew certs for hosts")
 #print(os.system('docker exec -d {} certbot --nginx -d aws.n3yron.ru -d petclinic.n3yron.ru --force-renewal -n'.format(nginx_container)))
-print("Reloading Nginx")
-print(os.system('docker exec -d {} nginx -s reload'.format(nginx_container)))
+
+print("Renew certs for hosts")
 proc1 = subprocess.Popen(['docker', 'exec', '-d', nginx_container, 'certbot', '--nginx', '-d', 'aws.n3yron.ru', '-d', 'petclinic.n3yron.ru', '--force-renewal', '-n'], stdout=subprocess.PIPE)
 output1 = proc1.stdout.readlines()
 for line in output1:
 	print(line)
 print(output1)
 
+print("Reloading Nginx")
+print(os.system('docker exec -d {} nginx -s reload'.format(nginx_container)))
 #certbot --nginx -d aws.n3yron.ru -d petclinic.n3yron.ru --force-renewal -n
