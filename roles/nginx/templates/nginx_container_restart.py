a97bb219e779e8a19ee0b81e016f6e25e7b778d3
n3yron <n3yron@gmail.com>
2021-01-03 12:45:22 +0200

    asdasd

--- roles/nginx/templates/nginx_container_restart.py
+++ roles/nginx/templates/nginx_container_restart.py
@@ -1,23 +1,25 @@
 #!/bin/python2
 import os, re, subprocess, shutil
 
-proc = subprocess.Popen(['docker', 'ps'], stdout=subprocess.PIPE)
-output = proc.stdout.readlines()
-for line in output:
-    if "nginx" in line:
-        nginx_container = line[0:12]
-print("Obtaining Nginx container id")
-print("Your Nginx container id is: "+nginx_container)
-#print("Renew certs for hosts")
-#print(os.system('docker exec -d {} certbot --nginx -d aws.n3yron.ru -d petclinic.n3yron.ru --force-renewal -n'.format(nginx_container)))
+def my_function():
+    try:
+    	proc = subprocess.Popen(['docker', 'ps'], stdout=subprocess.PIPE)
+        output = proc.stdout.readlines()
+        for line in output:
+            if "nginx" in line:
+                nginx_container = line[0:12]
+        print("Obtaining Nginx container id")
+        print("Your Nginx container id is: "+nginx_container)
+        print("Renew certs for hosts")
+        proc1 = subprocess.Popen(['docker', 'exec', '-d', nginx_container, 'certbot', '--nginx', '-d', 'aws.n3yron.ru', '-d', 'petclinic.n3yron.ru', '--force-renewal', '-n'], stdout=subprocess.PIPE)
+        output1 = proc1.stdout.readlines()
+        for line in output1:
+       	    print(line)
+            print(output1)
 
-print("Renew certs for hosts")
-proc1 = subprocess.Popen(['docker', 'exec', '-d', nginx_container, 'certbot', '--nginx', '-d', 'aws.n3yron.ru', '-d', 'petclinic.n3yron.ru', '--force-renewal', '-n'], stdout=subprocess.PIPE)
-output1 = proc1.stdout.readlines()
-for line in output1:
-	print(line)
-print(output1)
+        print("Reloading Nginx")
+        print(os.system('docker exec -d {} nginx -s reload'.format(nginx_container)))
+    except:
+    	print("Something go wrong.")
 
-print("Reloading Nginx")
-print(os.system('docker exec -d {} nginx -s reload'.format(nginx_container)))
-#certbot --nginx -d aws.n3yron.ru -d petclinic.n3yron.ru --force-renewal -n
+my_function()
